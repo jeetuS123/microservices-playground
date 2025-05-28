1176ae51a198826ff0a1731f1dbd358827b8cf8a
Jitendra Kumar <jitendrakumar@Jitendras-MacBook-Air.local>
2025-05-28 15:19:53 +0530

    Microservices connection

--- movie-catalog-service/src/main/java/io/konekt/movie_catalog_service/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/konekt/movie_catalog_service/MovieCatalogServiceApplication.java
@@ -2,9 +2,14 @@ package io.konekt.movie_catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- movie-catalog-service/src/main/java/io/konekt/movie_catalog_service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/konekt/movie_catalog_service/resources/MovieCatalogResource.java
@@ -1,21 +1,35 @@
 package io.konekt.movie_catalog_service.resources;
 
 import io.konekt.movie_catalog_service.models.CatalogItem;
+import io.konekt.movie_catalog_service.models.Movie;
+import io.konekt.movie_catalog_service.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @Autowired
+    RestTemplate restTemplate;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return List.of(
-                new CatalogItem("Inception", "A movie about dreams within dreams.", "****"),
-                new CatalogItem("The Shawshank Redemption", "A story about hope and friendship.", "**"),
-                new CatalogItem("The Dark Knight", "A superhero film featuring Batman.", "***")
+        List<Rating> ratings= Arrays.asList(
+                new Rating("123","5"),
+                new Rating("45","4"),
+                new Rating("67","3")
         );
+        return ratings.stream().map(rating->{
+             Movie movie=restTemplate.getForObject("http://localhost:8082/movies/"+rating.getId(),Movie.class);
+             return new CatalogItem(movie.getName(),"desc",rating.getRating());
+        }).collect(Collectors.toList());
+
+
     }
 }
